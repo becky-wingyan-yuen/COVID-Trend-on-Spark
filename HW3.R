5bc6b4d7a9fd24905e3c1ab3d819454e7c494598
Becky Yuen <wyyuen@terpmail.umd.edu>
2024-04-20 11:58:58 -0400

    .

--- HW3.R
+++ HW3.R
@@ -2,10 +2,9 @@ rm(list=ls())
 
 library(sparklyr)
 library(dplyr)
+library(tidyverse)
 
 sc <- spark_connect(master = "local")
-
-library(readr)
 
 urlfile = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv"
 urlfile1 = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
@@ -17,8 +16,8 @@ names(UID_ISO_FIPS_LookUp_Table)[which(names(UID_ISO_FIPS_LookUp_Table)=="Long_"
 names(time_series_covid19_confirmed_global)[which(names(time_series_covid19_confirmed_global)=="Province/State")] = "Province_State"
 names(time_series_covid19_confirmed_global)[which(names(time_series_covid19_confirmed_global)=="Country/Region")] = "Country_Region"
 
-#save(UID_ISO_FIPS_LookUp_Table, file = "UID_ISO_FIPS_LookUp_Table.csv")
-#save(time_series_covid19_confirmed_global, file = "time_series_covid19_confirmed_global.csv")
+save(UID_ISO_FIPS_LookUp_Table, file = "./data/UID_ISO_FIPS_LookUp_Table.csv")
+save(time_series_covid19_confirmed_global, file = "./data/time_series_covid19_confirmed_global.csv")
 
 
 #names(time_series_covid19_confirmed_global)[5:dim(time_series_covid19_confirmed_global)[2]] = 
@@ -30,10 +29,12 @@ LookUp_Table = copy_to(sc, UID_ISO_FIPS_LookUp_Table, overwrite = TRUE)
 time_series = copy_to(sc, time_series_covid19_confirmed_global, overwrite = TRUE)
 
 
-All_data = left_join(time_series, LookUp_Table, by = c("Country_Region","Lat","Long"))
+All_data = inner_join(time_series, LookUp_Table, by = c("Country_Region","Lat","Long"))
 
 All_data_country = sdf_copy_to(sc, All_data) %>%
-  filter(Country_Region%in%c("Germany","China","Japan","United Kingdom","US","Brail","Mexico"))
+  dplyr::filter(Country_Region%in%c("Germany","China","Japan","United Kingdom","US","Brazil","Mexico"))
+
+
 
 All_data_long_country = reshape(data = as_tibble(All_data_country),
                                 idvar= "UID",
@@ -44,10 +45,11 @@ All_data_long_country = reshape(data = as_tibble(All_data_country),
                                 new.row.names = 1:320040,
                                 direction = "long")
 
+table(All_data_long_country$Country_Region)
 
 
-save(All_data_country, file = "All_data_wide.csv")
-save(All_data_long_country, file = "All_data_long.csv")
+save(All_data_country, file = "./data/All_data_country_wide.csv")
+save(All_data_long_country, file = "./data/All_data_country_long.csv")
 
 
 
@@ -59,8 +61,30 @@ library(tidyr)
 library(dplyr)
 library(lubridate, warn.conflicts = FALSE)
 
-names(All_data_long_country)[which(names(All_data_long_country)=="date")] = "date_int"
-All_data_long_country$date = as.Date(as.numeric(All_data_long_country$date_int))
+temp_date = All_data_long_country$date
+
+for( i in 1:length(temp_date) ){
+  
+  if( str_length(temp_date[i]) == 4 ){
+    temp_date[i] = paste(c("0",substr(temp_date[i],1,1),"0",substr(temp_date[i],2,2),"20",substr(temp_date[i],3,4)), collapse="")
+  }else if( str_length(temp_date[i]) == 6 ){
+    temp_date[i] = paste(c(substr(temp_date[i],1,4),"20",substr(temp_date[i],5,6)), collapse="")
+  }else{  ## str_length(temp_date[i]) == 5 or 7
+    temp_date[i] = paste(c("0",substr(temp_date[i],1,3),"20",substr(temp_date[i],4,5)), collapse="")
+  }
+  
+}
+
+table(str_length(temp_date))
+
+All_data_long_country$date_int = as.numeric(mdy(temp_date))
+All_data_long_country$date = mdy(temp_date)
+
+
+sum(is.na(All_data_long_country$date))
+sum(is.na(All_data_long_country$Country_Region))
+sum(is.na(All_data_long_country$Population))
+sum(is.na(All_data_long_country$n_case))
 
 #All_data_long_country$n_case = as.numeric(All_data_long_country$n_case)
 
@@ -71,6 +95,7 @@ All_data_long_date_country = All_data_long_country %>%
   group_by(date, Country_Region) %>% 
   summarize(ncase_bydate = sum(n_case), total_population = sum(Population)) %>% 
   arrange(date) 
+
 
 
 graph2 = ggplot(data = All_data_long_date_country, mapping = aes(x = date, y = log(ncase_bydate)))
@@ -88,32 +113,35 @@ graph3 = ggplot(data = All_data_long_date_country, mapping = aes(x = date, y = n
 
 ggsave("fig3.jpeg", width = 10, height = 5)
 
+save(All_data_long_date_country, file = "./data/All_data_long_date_country_for_graph.csv")
 
 
 ###### Regression Analysis ###### 
 
+All_data_long_country_copy = All_data_long_country
 
 All_data_long_country$ndays_since_covid = as.numeric(All_data_long_country$date - sort(All_data_long_country$date)[1])
-All_data_long_country = All_data_long_country[-which(All_data_long_country$n_case==0),]
+All_data_long_country_2 = All_data_long_country[-which(All_data_long_country$n_case==0),]
 All_data_long_country$log_n_case = log(All_data_long_country$n_case)
 All_data_long_country = All_data_long_country[-which(All_data_long_country$log_n_case==0),]
 
-
+## US as reference
 All_data_long_country$China = (All_data_long_country$Country_Region=="China")+0
 All_data_long_country$Germany = (All_data_long_country$Country_Region=="Germany")+0
 All_data_long_country$Japan = (All_data_long_country$Country_Region=="Japan")+0
 All_data_long_country$Mexico = (All_data_long_country$Country_Region=="Mexico")+0
 All_data_long_country$UK = (All_data_long_country$Country_Region=="United Kingdom")+0
-All_data_long_country$US = (All_data_long_country$Country_Region=="US")+0
 
 
-All_data_long_country = All_data_long_country[,-which(colnames(All_data_long_country)%in%c("Province_State_x","FIPS","Admin2","Province_State_y","Country_Region", "Lat", "Long", "UID", "iso2", "iso3", "code3", "date_int", "n_case", "date", "Combined_Key"))]
-All_data_long_country = All_data_long_country[-which(is.na(All_data_long_country$Population)),]
+All_data_long_country_lm = All_data_long_country[,which(colnames(All_data_long_country)%in%
+                                                          c("Population","log_n_case","ndays_since_covid","China","Germany","Japan","Mexico","United Kingdom"))]
+All_data_long_country_lm = All_data_long_country_lm[-which(All_data_long_country_lm$log_n_case==-Inf),]
 
+save(All_data_long_country_lm, file = "./data/All_data_long_country_for_regression.csv")
 
-spark_All_data_long_country = copy_to(sc, All_data_long_country, overwrite = TRUE)
+spark_data_for_regrssion = copy_to(sc, All_data_long_country_lm, overwrite = TRUE)
 
-model = spark_All_data_long_country %>% ml_linear_regression(log_n_case ~ .) 
+model = spark_data_for_regrssion %>% ml_linear_regression(log_n_case ~ .) 
 summary(model)
 
 
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # SURV675-S2024-Assignment3
+
+
+
+### Session Info
+
+
+```
+sessionInfo()
+
+R version 4.2.2 Patched (2022-11-10 r83330)
+Platform: x86_64-pc-linux-gnu (64-bit)
+Running under: Ubuntu 20.04.4 LTS
+
+Matrix products: default
+BLAS:   /usr/lib/x86_64-linux-gnu/blas/libblas.so.3.9.0
+LAPACK: /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.9.0
+
+locale:
+ [1] LC_CTYPE=C.UTF-8       LC_NUMERIC=C           LC_TIME=C.UTF-8        LC_COLLATE=C.UTF-8     LC_MONETARY=C.UTF-8    LC_MESSAGES=C.UTF-8    LC_PAPER=C.UTF-8      
+ [8] LC_NAME=C              LC_ADDRESS=C           LC_TELEPHONE=C         LC_MEASUREMENT=C.UTF-8 LC_IDENTIFICATION=C   
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+ [1] forcats_1.0.0   stringr_1.5.1   purrr_1.0.2     readr_2.1.5     tibble_3.2.1    tidyverse_2.0.0 sparklyr_1.8.5  lubridate_1.9.3 dplyr_1.1.4     tidyr_1.3.1    
+[11] ggplot2_3.5.0  
+
+loaded via a namespace (and not attached):
+ [1] pillar_1.9.0      compiler_4.2.2    dbplyr_2.5.0      tools_4.2.2       uuid_1.1-0        bit_4.0.4         jsonlite_1.8.8    lifecycle_1.0.3   gtable_0.3.1     
+[10] timechange_0.1.1  pkgconfig_2.0.3   rlang_1.1.3       DBI_1.1.3         cli_3.6.2         rstudioapi_0.14   parallel_4.2.2    yaml_2.3.6        withr_2.5.0      
+[19] httr_1.4.4        systemfonts_1.0.4 generics_0.1.3    vctrs_0.6.5       askpass_1.1       hms_1.1.2         bit64_4.0.5       grid_4.2.2        tidyselect_1.2.1 
+[28] glue_1.6.2        R6_2.5.1          textshaping_0.3.6 fansi_1.0.3       vroom_1.6.0       farver_2.1.1      blob_1.2.3        tzdb_0.3.0        magrittr_2.0.3   
+[37] scales_1.3.0      ellipsis_0.3.2    colorspace_2.0-3  ragg_1.3.0        labeling_0.4.2    config_0.3.2      utf8_1.2.2        stringi_1.7.8     openssl_2.0.4    
+[46] munsell_0.5.0     crayon_1.5.2
+
+```
